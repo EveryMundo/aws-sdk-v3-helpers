fbb3e3bcb511ebb2e19457d1de3fe01ed6902051
Daniel Santana <daniel@everymundo.com>
2023-09-22 13:37:18 -0400

    Add a kinesis client with putRecord & putRecords methods

---
+++ kinesis/client.mjs
@@ -0,0 +1,36 @@
+import {
+  KinesisClient,
+  PutRecordCommand,
+  PutRecordsCommand
+} from '@aws-sdk/client-kinesis'
+
+import { asyncGunzip, asynGzip } from '../lib/zipper.mjs'
+
+export const createHelper = (KinesisClientClass = KinesisClient) => ({
+  _client: undefined,
+  get client () {
+    if (this._client == null) {
+      this._client = new KinesisClientClass({ region: process.env.AWS_REGION })
+    }
+
+    return this._client
+  },
+
+  putRecord (params) {
+    return this.client.send(new PutRecordCommand(params))
+  },
+
+  putRecords (params) {
+    return this.client.send(new PutRecordsCommand(params))
+  }
+})
+
+export const client = createHelper()
+export const createKinesisHelper = createHelper
+
+export default {
+  client,
+  kinesis: client,
+  createHelper,
+  createKinesisHelper: createHelper
+}
